2ced2620e68f2e4ea1b46401f2b63dcc5a29443a
YashDoesCode <theyashsrivastava07+git@gmail.com>
2024-10-28 00:06:17 +0530

    27/10/2024

---
+++ Friends/Siddharth_Gautam/Node.java
@@ -0,0 +1,12 @@
+package Friends.Siddharth_Gautam;
+
+class Node
+{
+    int data;
+    Node link;
+    Node()
+    {
+        data=0;
+        link=null;
+    }
+}
---
+++ Friends/Siddharth_Gautam/Plane.java
@@ -0,0 +1,17 @@
+package Friends.Siddharth_Gautam;
+
+import java.util.*;
+class Plane
+{
+    int x1,y1;
+    Plane(int nx, int ny)
+    {
+        x1=nx;
+        y1=ny;
+    }
+    void Show()
+    {
+        System.out.println("x=coordinte : " + x1);
+        System.out.println("y=coordinte : " + y1);
+    }
+}
